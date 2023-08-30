314eec736f6fa47d541796e99ccfd078cd32d70e
bparlan <bparlan@gmail.com>
2023-08-30 12:56:47 +0300

    content.js foxed due to fsevents dep

--- routes/content.js
+++ routes/content.js
@@ -112,18 +112,15 @@ router.post("/updateContent/:id", uploadMultiple, async (req, res) => {
     const { id } = req.params;
 
     // if (req.files) {
-    const checkSpaceExist = await content.findOne({ _id: id });
-    console.log("checkSpaceExist checkSpaceExist", checkSpaceExist);
-
     const urlOne = req.files.picture1
       ? `uploads/${req.files.picture1[0].filename}`
-      : checkSpaceExist?.picture1;
+      : (checkSpaceExist && checkSpaceExist.picture1) || null;
     const urlSecond = req.files.picture2
       ? `uploads/${req.files.picture2[0].filename}`
-      : checkSpaceExist?.picture2;
+      : (checkSpaceExist && checkSpaceExist.picture2) || null;
     const urlthird = req.files.picture3
       ? `uploads/${req.files.picture3[0].filename}`
-      : checkSpaceExist?.picture3;
+      : (checkSpaceExist && checkSpaceExist.picture3) || null;
     console.log("urlOne", urlOne);
     console.log("urlSecond", urlOne);
     console.log("urlthird", urlthird);
